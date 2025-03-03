b05b69d03d26d98d1d70f7516f03dcf3ff80c31c
Aditya Das <adityabkp1234@gmail.com>
2025-03-03 23:48:43 +0530

    dependency config

--- lib/configure_dependency.dart
+++ lib/configure_dependency.dart
@@ -2,11 +2,21 @@ import 'package:get_it/get_it.dart';
 import 'package:rideinsync_client/services/cloudinary_service.dart';
 import 'package:rideinsync_client/services/common_auth_service.dart';
 import 'package:rideinsync_client/services/firebase_service.dart';
+import 'package:rideinsync_client/services/location_autocomplete_service.dart';
 
 final locator = GetIt.instance;
 
 void configureDependency() {
-  locator.registerLazySingleton<CommonAuthService>(() => CommonAuthService());
-  locator.registerLazySingleton<FirebaseService>(() => FirebaseService());
-  locator.registerLazySingleton<CloudinaryService>(() => CloudinaryService());
+  locator.registerLazySingleton<CommonAuthService>(
+    () => CommonAuthService(),
+  );
+  locator.registerLazySingleton<FirebaseService>(
+    () => FirebaseService(),
+  );
+  locator.registerLazySingleton<CloudinaryService>(
+    () => CloudinaryService(),
+  );
+  locator.registerLazySingleton<LocationAutoCompleteService>(
+    () => LocationAutoCompleteService(),
+  );
 }
